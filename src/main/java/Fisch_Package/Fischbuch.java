62dbe11787288a198f43b177a31b0624449f813e
bjoern2121 <bjoooern.s@gmail.com>
2025-06-03 16:35:13 +0200

    Test commit

--- src/main/java/Fisch_Package/Fischbuch.java
+++ src/main/java/Fisch_Package/Fischbuch.java
@@ -1,16 +1,4 @@
 package Fisch_Package;
 
-import javax.swing.*;
-
 public class Fischbuch {
-    private JComboBox comboBox1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JLabel FIschart_textField;
-    private JTextField textField4;
-    private JRadioButton jaRadioButton;
-    private JButton eintragenButton;
-    private JButton fangbuchAnzeigenButton;
-    private JButton auswahlLöschenButton;
 }
